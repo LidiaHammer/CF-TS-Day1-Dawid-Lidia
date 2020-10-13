b742cfef514bc037eff85572e858437127608f9c
Lidia Cekic <lidiacekic9@gmail.com>
2020-10-13 15:18:42 +0200

    done basic exercise

--- basic/basic.js
+++ basic/basic.js
@@ -1,2 +1,37 @@
-var message = 'Hello World';
-console.log(message);
+// basic 1
+// 'use strict';
+// let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// for (let i = 1; i < array.length; i++)
+//   for (var value of array) {
+//     console.log(value + 'x' + i + '=' + value * i);
+//   }
+// basic 2
+// let array = ['Lidia', 'Dawid'];
+// array.forEach(function (value) {
+//   document.write(value);
+// });
+// for (let i = 0; i < 10; i++) {
+//   document.write('<br>', array[0]);
+// }
+// setTimeout(function () {
+//   for (let i = 0; i < 10; i++) {
+//     document.write('<br>', array[0]);
+//     document.write('<br>', array[1]);
+//   }
+// }, 3000);
+// basic 3
+// displays the value of the array:
+// let word: Array<string> = ['abc', 'sab', 'das'];
+// for (let hallo of word) {
+//   console.log(hallo);
+// }
+// displays the index of the array:
+// let word: Array<string> = ['abc', 'sab', 'das'];
+// for (let hallo in word) {
+//   console.log(hallo);
+// }
+// displays the index and value of the array:
+// let word: Array<string> = ['abc', 'sab', 'das'];
+// for (let hallo in word) {
+//   console.log(word[hallo], hallo);
+// }
